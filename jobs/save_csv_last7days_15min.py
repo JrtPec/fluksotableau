7bba790a8d775e77b6b5788bfc971d6bab79a06a
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-10-13 13:00:09 +0200

    sync 15 min

---
+++ jobs/save_csv_last7days_15min.py
@@ -0,0 +1,48 @@
+import os, inspect, sys, time
+import pandas as pd
+import filecmp
+
+script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.append(os.path.join(script_dir, os.pardir, os.pardir))
+
+from fluksotableau.library import config, datalayer
+from fluksotableau.library import metadata2 as md
+c = config.Config()
+
+path_to_data = c.get('data', 'csvfolder2')
+if not os.path.exists(path_to_data):
+    raise IOError("Provide your path to the data in the config file")
+
+sys.path.append(c.get('tmpo','folder'))
+import tmpo
+
+metadata = md.Metadata()
+fluksos = metadata.fluksos
+
+try:
+	tmpos = tmpo.Session(path = c.get('tmpo','path'))
+except:
+	tmpos = tmpo.Session()
+
+dl = datalayer.DataLayer(tmpos)
+
+for f in fluksos:
+    for s in f.sensors:
+        ts = dl.tmpo_dataframe([s.sensor_id], head = pd.Timestamp('now').normalize() - pd.Timedelta(days=7))
+        if ts is None: continue
+        ts.columns = ['consumption']
+        ts['meterID'] = s.sensor_id
+
+        ts = ts.resample('15min', how='mean')
+
+        for group in ts.groupby(ts.index.day):
+        	filename = "15min.{}.{}.csv".format(s.sensor_id,group[1].first_valid_index().date())
+        	#save file locally
+        	group[1].to_csv('temp2.csv')
+
+        	if os.path.isfile(os.path.join(path_to_data,filename)) and (filecmp.cmp('temp.csv', os.path.join(path_to_data, filename))):
+        		print "file already exists, not saving"
+        		continue
+        	else:
+        		print "saving new file"
+        	group[1].to_csv(os.path.join(path_to_data, filename))
